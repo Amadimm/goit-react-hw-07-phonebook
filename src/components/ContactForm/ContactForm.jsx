49686ecd04935fb167bb9ffc7d497df750205949
Amadimm <amadeuszrolecki@gmail.com>
2023-08-05 03:59:55 +0200

    ff

---
+++ src/components/ContactForm/ContactForm.jsx
@@ -0,0 +1,66 @@
+import { useDispatch, useSelector } from 'react-redux';
+import css from './ContactForm.module.css';
+import { addContact } from 'redux/operations';
+import { selectContacts } from 'redux/selectors';
+
+export const ContactForm = () => {
+  const dispatch = useDispatch();
+
+  const stateContacts = useSelector(selectContacts);
+  const stateContactsNames = stateContacts.map(contact => contact.name);
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    const form = e.target;
+    const name = form.elements.name.value;
+    const phone = form.elements.phone.value;
+
+    const contact = {
+      name,
+      phone,
+    };
+
+    if (stateContactsNames.includes(name)) {
+      form.reset();
+      return alert(`${name} is alredy in contacts`);
+    }
+
+    dispatch(addContact(contact));
+
+    form.reset();
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit} className={css.form}>
+        <label className={css.label}>
+          Name
+          <input
+            type="text"
+                      name="name"
+                       pattern="^[A-Za-z.'\- ]+$"
+            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+            required
+            className={css.input}
+          />
+        </label>
+
+        <label className={css.label}>
+          Number
+          <input
+            type="tel"
+                      name="phone"
+                       pattern="^\+?\d{1,4}?\s?\(?\d{1,4}?\)?\s?\d{1,4}\s?\d{1,4}\s?\d{1,9}$"
+            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+            required
+            className={css.input}
+          />
+        </label>
+
+        <button type="submit" className={css.btn}>
+          Add contact
+        </button>
+      </form>
+    </div>
+  );
+};
